e596cd8cc021739712c239dc81548689f4626b8f
chaosannals <chaosannals@outlook.com>
2022-02-08 14:28:38 +0800

    fmt

--- webroot/main.go
+++ webroot/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"os"
+	"fmt"
 	"path/filepath"
 
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,7 @@ func main() {
 	if len(os.Args) > 1 {
 		d = filepath.Join(d, os.Args[1])
 	}
+	fmt.Println("http://localhost:50000")
 	r.StaticFS("/", http.Dir(d))
 	r.Run(":50000")
 }
