99c199077112acc06d74112265b587d7badcbbc6
chaosannals <chaosannals@outlook.com>
2022-10-29 14:59:59 +0800

    save

--- olddemo/aes256/main.go
+++ olddemo/aes256/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 
 	"github.com/andreburgaud/crypt2go/ecb"
+	"github.com/andreburgaud/crypt2go/padding"
 )
 
 const (
@@ -37,17 +38,71 @@ func (info *DataJsonInfo) Encode() (string, error) {
 	if err != nil {
 		return "", err
 	}
+
 	log.Println(string(raw))
+
 	// 256 / 8 = 32 库按 key 长度使用 128 或 256 位算法
 	block, err := aes.NewCipher(PaddingZero([]byte(ENCODE_KEY), 32))
 	if err != nil {
 		return "", err
 	}
 	mode := ecb.NewECBEncrypter(block)
-	bs := PaddingZero(raw, mode.BlockSize())
+
+	log.Printf("encode: raw len: %d", len(raw))
+
+	// bs := PaddingZero(raw, mode.BlockSize())
+
+	// 这个库还提供了一些补齐方法。
+	padder := padding.NewPkcs7Padding(mode.BlockSize())
+	bs, err := padder.Pad(raw)
+	if err != nil {
+		return "", err
+	}
+
+	log.Printf("encode: bs len: %d %% %d \n", len(bs), mode.BlockSize())
+
 	r := make([]byte, len(bs))
 	mode.CryptBlocks(r, bs)
+
+	log.Printf("encode: len: %d \n", len(r))
 	return base64.StdEncoding.EncodeToString(r), nil
+}
+
+func Decode(encodeText string) (*DataJsonInfo, error) {
+	raw, err := base64.StdEncoding.DecodeString(encodeText)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("decode: len: %d \n", len(raw))
+
+	block, err := aes.NewCipher(PaddingZero([]byte(ENCODE_KEY), 32))
+	if err != nil {
+		return nil, err
+	}
+
+	mode := ecb.NewECBDecrypter(block)
+	bs := make([]byte, len(raw))
+	mode.CryptBlocks(bs, raw)
+
+	log.Printf("decode: bs len: %d %% %d \n", len(bs), mode.BlockSize())
+
+	padder := padding.NewPkcs7Padding(mode.BlockSize())
+	r, err := padder.Unpad(bs)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// r := bs[:len(bs)-4]
+
+	log.Printf("decode: r len: %d", len(r))
+
+	var di DataJsonInfo
+	if err := json.Unmarshal(r, &di); err != nil {
+		return nil, err
+	}
+	return &di, nil
 }
 
 func main() {
@@ -56,5 +111,15 @@ func main() {
 		String2: "2022-10-29 12:00:12",
 		Int1:    12354678,
 	}
-	log.Println(data.Encode())
+	ed,err := data.Encode()
+	if err !=nil {
+		log.Printf("error1: %v \n", err)
+	}
+	log.Println(ed)
+
+	r, err := Decode(ed)
+	if err !=nil {
+		log.Printf("error2: %v \n", err)
+	}
+	log.Println(r)
 }
