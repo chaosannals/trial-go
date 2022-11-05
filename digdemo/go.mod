2cc1d26ea73750eaf3d822a4a934d09716fc74ac
chaosannals <chaosannals@outlook.com>
2022-11-05 15:09:58 +0800

    lumberjack

--- digdemo/go.mod
+++ digdemo/go.mod
@@ -3,9 +3,15 @@ module github.com/chaosannals/trial-go-digdemo
 go 1.18
 
 require (
+	github.com/rs/zerolog v1.28.0
+	go.uber.org/dig v1.15.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
-	github.com/rs/zerolog v1.28.0 // indirect
-	go.uber.org/dig v1.15.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- digdemo/trial/logging.go
+++ digdemo/trial/logging.go
@@ -4,20 +4,30 @@ import (
 	"os"
 
 	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func NewZLogger() (*zerolog.Logger, error) {
 	// 命令行输出
 	cw := zerolog.ConsoleWriter{Out: os.Stdout}
 
-	// 文件输出 TODO 这个要找个能文件切割的
+	// 简单文件输出
 	fw, err := os.OpenFile("debug.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return nil, err
 	}
 
+	// 带文件切割和压缩的输出
+	lw := &lumberjack.Logger{
+		Filename:   "./log/debug.log",
+		MaxSize:    500, // megabytes
+		MaxBackups: 3,
+		MaxAge:     28,   //days
+		Compress:   true, // disabled by default
+	}
+
 	// 多输出端
-	m := zerolog.MultiLevelWriter(cw, fw)
+	m := zerolog.MultiLevelWriter(cw, fw, lw)
 	logger := zerolog.New(m).With().Timestamp().Logger()
 	return &logger, err
 }
