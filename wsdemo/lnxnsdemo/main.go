04206656f566cc788daecaf6f6626a080cc7e0f5
chaosannals <chaosannals@outlook.com>
2023-05-23 17:34:58 +0800

    save

--- wsdemo/lnxnsdemo/main.go
+++ wsdemo/lnxnsdemo/main.go
@@ -13,13 +13,27 @@ import (
 /// 启动一个 sh 并进入交互模式，只能在 linux 下执行，调用了 linux 系统方法。要 root
 func main() {
 	simple.DoSome()
-	cmd := exec.Command("sh")
+	cmd := exec.Command("/bin/sh")
 
 	// syscall.CLONE_NEWUTS 克隆出新的 UTS 此时 hostname 是隔离的
 	// syscall.CLONE_NEWIPC 克隆出新的 IPC
+	// syscall.CLONE_NEWPID PID 独立 此时 sh 内部查看自身 PID 为 1
+	// syscall.CLONE_NEWNS Mount 名字空间化， mount 和 unmount 挂载的空间独立
+	// syscall.CLONE_NEWUSER 用户独立，加上后 sh 用户从 root 变成 nobody
+	// syscall.CLONE_NEWNET 网络
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+		// Setpgid: true,
+        // Pgid: 0,
+		GidMappingsEnableSetgroups: true,
+
+		// 设置了 syscall.CLONE_NEWUSER 这个一设置就有权限问题
+		// Credential: &syscall.Credential {
+		// 	Uid: uint32(1),
+		// 	Gid: uint32(1),
+		// },
 	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
--- wsdemo/readme.md
+++ wsdemo/readme.md
@@ -1,4 +1,6 @@
-# Linux Namespace Demo
+# Linux 容器 Demo
+
+## Namespace 名字空间【隔离】
 
 ```bash
 ## 打印当前进程树
@@ -27,3 +29,43 @@ ipcs -u
 ## 创建 IPC
 ipcmk -Q
 ```
+
+```bash
+## 查看系统信息
+ls /proc
+
+## 查看进程信息
+ps -ef
+
+## 指定类型 -t proc；  当前 proc 挂载到 /proc
+mount -t proc proc /proc
+```
+
+```bat
+@rem 通过 root 启动 wsl
+wsl -u root
+```
+
+```bash
+## 查询当前用户
+whoami
+
+## 查看当前用户 id 信息
+id
+
+## 查看用户信息文件
+cat /etc/passwd
+```
+
+```bash
+# 查看网络地址
+ip address
+
+# 查看网络路由
+ip route
+
+# 查看网络(这个比较老的命令，可能新系统没有要装 net-tools)
+ifconfig
+```
+
+## Cgroups 资源【限制】
