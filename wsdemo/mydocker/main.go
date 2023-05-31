ae88f5c34232497a4f12736e3aa5e70ebc9475f8
chaosannals <chaosannals@outlook.com>
2023-05-31 11:20:51 +0800

    save

---
+++ wsdemo/mydocker/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"os"
+	"fmt"
+)
+
+const usage = `my docker usage.`
+
+var runCommand = cli.Command {
+	Name: "run",
+	Usage: `my docker run.`,
+	Flags: []cli.Flag {
+		cli.BoolFlag {
+			Name: "ti",
+			Usage: "enable tty",
+		},
+	},
+	Action: func(context *cli.Context) error {
+		if len(context.Args()) < 1 {
+			return fmt.Errorf("Missing container command")
+		}
+		//cmd := context.Args().Get(0)
+		//tty := context.Bool("ti")
+		//Run(tty, cmd)
+		return nil
+	},
+}
+
+var initCommand = cli.Command {
+	Name: "init",
+	Usage: "init container",
+	Action: func (context * cli.Context) error {
+		log.Infof("init come on")
+		cmd := context.Args().Get(0)
+
+		log.Infof("command %s", cmd)
+		return nil
+		//err := container.RunContainerInitProcess(cmd, nil)
+		//return err
+	},
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "mydocker"
+	app.Usage = usage
+
+	app.Commands = []cli.Command {
+		initCommand,
+		runCommand,
+	}
+
+	app.Before = func(context *cli.Context) error {
+		log.SetFormatter(&log.JSONFormatter{})
+		log.SetOutput(os.Stdout)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
