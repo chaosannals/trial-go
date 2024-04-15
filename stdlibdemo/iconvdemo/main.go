8bb4a46dabca9372139bfb45afdbda3596c4c0be
chaosannals <chaosannals@outlook.com>
2024-04-15 17:47:00 +0800

    save

--- stdlibdemo/iconvdemo/main.go
+++ stdlibdemo/iconvdemo/main.go
@@ -7,10 +7,19 @@ import (
 	"log"
 
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
 )
 
 func Utf8ToGbk(text string) (string, error) {
+	r := bytes.NewReader([]byte(text))
+	decoder := transform.NewReader(r, simplifiedchinese.GBK.NewEncoder())
+	result, err := io.ReadAll(decoder)
+	fmt.Printf("result: %d\n", len(result))
+	return string(result), err
+}
+
+func GbkToUtf8(text string) (string, error) {
 	r := bytes.NewReader([]byte(text))
 	decoder := transform.NewReader(r, simplifiedchinese.GBK.NewDecoder())
 	result, err := io.ReadAll(decoder)
@@ -18,14 +27,42 @@ func Utf8ToGbk(text string) (string, error) {
 	return string(result), err
 }
 
+func Utf8ToUtf16Le(text string) (string, error) {
+	r := bytes.NewReader([]byte(text))
+	decoder := transform.NewReader(r, unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewEncoder())
+	result, err := io.ReadAll(decoder)
+	fmt.Printf("result: %d\n", len(result))
+	return string(result), err
+}
+
+func Utf16LeToUtf8(text string) (string, error) {
+	result, _, err := transform.Bytes(unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder(), []byte(text))
+	return string(result), err
+}
+
 func main() {
 	fmt.Println("start")
-	a := "ROOT ENTRY 中文"
-	b, err := Utf8ToGbk(a)
+	utf8Text := "ROOT ENTRY 中文"
+	gbkText, err := Utf8ToGbk(utf8Text)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("a: %d b: %d\n", len(a), len(b))
-	fmt.Printf("a: %s b: %s\n", a, b)
+	gbkTextBack, err := GbkToUtf8(gbkText)
+	if err != nil {
+		log.Fatal(err)
+	}
+	utf16LeText, err := Utf8ToUtf16Le(utf8Text)
+	if err != nil {
+		log.Fatal(err)
+	}
+	utf16LeTextBack, err := Utf16LeToUtf8(utf16LeText)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("utf8Text: %d %s\n", len(utf8Text), utf8Text)
+	fmt.Printf("utf16LeText: %d %s\n", len(utf16LeText), utf16LeText)
+	fmt.Printf("utf16LeTextBack: %d %s\n", len(utf16LeTextBack), utf16LeTextBack)
+	fmt.Printf("gbkText: %d %s\n", len(gbkText), gbkText)
+	fmt.Printf("gbkTextBack: %d %s\n", len(gbkTextBack), gbkTextBack)
 	fmt.Printf("len: %d\n", len("SUMMARYINFORMATION"))
 }
--- xlsrd2/xlsrd2/book.go
+++ xlsrd2/xlsrd2/book.go
@@ -1,6 +1,8 @@
 package xlsrd2
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type XlsBookProperty struct {
 	TypeId   int
@@ -27,11 +29,12 @@ type XlsBook struct {
 	bigBlockChain       []byte
 	smallBlockChain     []byte
 	entry               []byte
-	xlsBytes            []byte
+	xlsBytes            []byte // 对应 OLERead 类 data
 	PropertySets        *XlsBookPropertySets
-	Data                []byte
+	Data                []byte // 对应 xls 类 data
 	SummaryInfo         []byte
 	DocumentSummaryInfo []byte
+	version int16
 }
 
 func (i *XlsBook) ReadStream(id int32) ([]byte, error) {
@@ -93,16 +96,124 @@ func (i *XlsBook) ReadStream(id int32) ([]byte, error) {
 	return streamData, nil
 }
 
-// func (i *XlsBook) ListWookSheetNames() ([]string, error) {
-// 	dLen := len(i.data)
-// 	pos := 0
-// 	sheets := make([]string, 0)
+func (i *XlsBook) ListWookSheetNames() ([]string, error) {
+	dLen := len(i.Data)
+	pos := 0
+	sheets := make([]string, 0)
 
-// 	for ;pos < dLen; {
-// 		code, err := GetUInt2d(i.data, pos)
-// 		if err != nil {
-// 			return sheets, err
-// 		}
+	for pos < dLen {
+		code, err := GetUInt2d(i.Data, pos)
+		if err != nil {
+			return sheets, err
+		}
+		switch code {
+		case XLS_TYPE_BOF:
+			p, err := i.ReadBof(pos)
+			if err != nil {
+				return sheets, err
+			}
+			pos = p
+			break
+		case XLS_TYPE_SHEET:
+			p, err := i.ReadSheet(pos)
+			if err != nil {
+				return sheets, err
+			}
+			pos = p
+			break
+		}
+	}
+}
 
-// 	}
-// }
+func (i *XlsBook) ReadBof(pos int) (int, error) {
+	length, err := GetUInt2d(i.Data, pos+2)
+	if err != nil {
+		return pos, err
+	}
+
+	recordData := i.Data[pos+4 : length]
+	pos += 4 + int(length)
+	substreamType, err := GetUInt2d(recordData, 2)
+	if err != nil {
+		return pos, err
+	}
+
+	switch substreamType {
+	case XLS_WORKBOOKGLOBALS:
+		version, err := GetUInt2d(recordData, 0)
+		if err != nil {
+			return pos, err
+		}
+		if (version != XLS_BIFF8) && (version != XLS_BIFF7) {
+			return pos, fmt.Errorf("这个文件的格式太老了")
+		}
+		// TODO 这个结构要读到这一步才能获取到版本信息，加载流程确认下
+		fmt.Printf("xls 文件版本: %d", version)
+		i.version = version
+		break
+	case XLS_WORKSHEET:
+		// 此项的版本信息不可靠
+		// (OpenOffice doc, 5.8)指出使用全局流里面的版本信息
+		break
+	default:
+		for {
+			code, err := GetUInt2d(i.Data, pos)
+			if err != nil {
+				return pos, err
+			}
+			p, err := i.ReadDefault()
+			if err != nil {
+				return p, err
+			}
+			pos = p
+			if code == XLS_TYPE_EOF || pos >= len(i.Data) {
+				break
+			}
+		}
+		break
+	}
+	return pos, nil
+}
+
+func (i *XlsBook) ReadSheet(pos int)  (int, error) {
+	length, err := GetUInt2d(i.Data, pos+2)
+	if err != nil {
+		return pos, err
+	}
+	recordData := i.Data[pos+4 : length]
+
+	recOffset, err := GetInt4d(i.Data, pos + 4)
+	if err != nil {
+		return pos, err
+	}
+	var sheetState string
+	switch recordData[4] {
+	case 0x00:
+		sheetState = WORKSHEET_SHEETSTATE_VISIBLE
+		break
+	case 0x01:
+		sheetState = WORKSHEET_SHEETSTATE_HIDDEN
+		break
+	case 0x02:
+		sheetState = WORKSHEET_SHEETSTATE_VERYHIDDEN
+		break
+	default:
+		sheetState = WORKSHEET_SHEETSTATE_VISIBLE
+		break
+	}
+	fmt.Printf("sheet state: %s", sheetState)
+
+	sheetType := recordData[5]
+	fmt.Printf("sheet type: %d", sheetType)
+
+	recName := ""
+	switch i.version {
+	case XLS_BIFF8:
+		recName = ""
+	}
+
+}
+
+func (i *XlsBook) ReadDefault() (int, error) {
+
+}
