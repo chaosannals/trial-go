138e3f8a86d39abb7b570fc446c4d3d0683c8814
chaosannals <chaosannals@outlook.com>
2024-07-07 14:34:34 +0800

    disk.

--- lib3thdemo/gopsutildemo/main.go
+++ lib3thdemo/gopsutildemo/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/disk"
 	"github.com/shirou/gopsutil/v4/mem"
 )
 
@@ -20,6 +21,7 @@ func main() {
 		fmt.Println(v)
 	}
 
+	fmt.Println("====================================")
 	// CPU
 	if v, err := cpu.Info(); err != nil {
 		log.Fatalln(err)
@@ -27,10 +29,38 @@ func main() {
 		fmt.Println(v)
 	}
 
+	fmt.Println("====================================")
 	// CPU 占用，是否拆分
 	if v, err := cpu.Times(false); err != nil {
 		log.Fatalln(err)
 	} else {
 		fmt.Println(v)
+	}
+
+	fmt.Println("====================================")
+	// 硬盘分区
+	if dps, err := disk.Partitions(true); err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println(dps)
+		for i, dp := range dps {
+			// Windows 下调用提示没有实现。not implemented yet
+			// if sn, err := disk.SerialNumber(dp.Mountpoint); err != nil {
+			// 	log.Fatalln(err)
+			// } else {
+			// 	fmt.Printf("[%d] %v\n", i, sn)
+			// }
+			// Windows 下调用提示没有实现。not implemented yet
+			// if dl, err := disk.Label(dp.Mountpoint); err != nil {
+			// 	log.Fatalln(err)
+			// } else {
+			// 	fmt.Printf("[%d] %v\n", i, dl)
+			// }
+			if du, err := disk.Usage(dp.Mountpoint); err != nil {
+				log.Fatalln(err)
+			} else {
+				fmt.Printf("[%d] %v\n", i, du)
+			}
+		}
 	}
 }
